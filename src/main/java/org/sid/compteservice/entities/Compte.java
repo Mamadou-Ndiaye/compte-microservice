eaa3268817a86840c925bad07f83b6afb6d4d449
Mamadou  NDIAYE <mamadou.ndiaye@gainde2000.sn>
2021-12-08 11:06:17 +0000

    Add lombok and change database a H Memory in app.propreties and pom.xml

--- src/main/java/org/sid/compteservice/entities/Compte.java
+++ src/main/java/org/sid/compteservice/entities/Compte.java
@@ -1,14 +1,21 @@
 package org.sid.compteservice.entities;
 
+//import lombok.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.sid.compteservice.enums.TypeCompte;
 
 import javax.persistence.*;
 import java.util.Date;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 @Entity
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data
 public class Compte {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long code;
@@ -16,4 +23,56 @@ public class Compte {
     private Date dateCreation;
     @Enumerated(EnumType.STRING)
     private TypeCompte type;
+
+   /* public Compte() {
+    }
+
+    public Compte(Long code, double solde, Date dateCreation, TypeCompte type) {
+        this.code = code;
+        this.solde = solde;
+        this.dateCreation = dateCreation;
+        this.type = type;
+    }
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public TypeCompte getType() {
+        return type;
+    }
+
+    public void setType(TypeCompte type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Compte{" +
+                "code=" + code +
+                ", solde=" + solde +
+                ", dateCreation=" + dateCreation +
+                ", type=" + type +
+                '}';
+    }*/
 }
